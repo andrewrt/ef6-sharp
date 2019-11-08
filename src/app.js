f29973e1a5e780928106cd987b6669624d4928e5
andrew <andrew.temple@variabletech.com>
2019-11-08 17:09:59 -0500

    remove extraneous logs

--- src/app.js
+++ src/app.js
@@ -39,17 +39,14 @@ const semiTransparentRedPng = sharp({
       width: 48,
       height: 48,
       channels: 4,
-      background: { r: 255, g: 0, b: 0, alpha: 0.5 }
+      background: { r: 111, g: 0, b: 255, alpha: 0.5 }
     }
   }).png().toFile('preview.png', (err, info) => {
     if (err) {
       console.log('err in test.png: ', err);
-      Alert('USB RX JSON Error', err.message, 'error');
     }
     if (info) {
       console.log('test.png info: ', info);
-      console.log(this.imageType)
-      this.emitter.emit("captured_image", { imageType: this.imageType, imageHash: Date.now().toString(), imagePath, histogram, histoPath })
     }
   });
  
